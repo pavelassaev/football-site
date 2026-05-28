9aae74d047aa2ac9dc7385f73b6a92b89a9ed5b1
Pavel Assaev <286280683+pavelassaev@users.noreply.github.com>
2026-05-28 09:00:26 +0500

    Add dark football theme CSS

--- main.go
+++ main.go
@@ -7,18 +7,19 @@ import (
 )
 
 func main() {
-	// Обработчик главной страницы
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Загружаем шаблон главной страницы
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			http.Error(w, "Ошибка загрузки страницы", http.StatusInternalServerError)
 			fmt.Println("Ошибка шаблона:", err)
 			return
 		}
-		// Отдаём страницу пользователю
 		tmpl.Execute(w, nil)
 	})
+
+	// Раздаём статические файлы (CSS, картинки) из папки static
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	fmt.Println("Сервер запущен на http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
