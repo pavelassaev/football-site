2bf859b66007d420c0410e8e699c16eb590ad6e0
Pavel Assaev <286280683+pavelassaev@users.noreply.github.com>
2026-05-28 08:55:02 +0500

    Add HTML templates structure with home page

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
+	// Обработчик главной страницы
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "<h1>Сайт футбольной аналитики</h1>")
-		fmt.Fprintln(w, "<p>Скоро здесь будет полноценный портал про КПЛ.</p>")
+		// Загружаем шаблон главной страницы
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, "Ошибка загрузки страницы", http.StatusInternalServerError)
+			fmt.Println("Ошибка шаблона:", err)
+			return
+		}
+		// Отдаём страницу пользователю
+		tmpl.Execute(w, nil)
 	})
 
 	fmt.Println("Сервер запущен на http://localhost:8080")
